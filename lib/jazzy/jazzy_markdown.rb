2ea4df88d096d086db02e66f651a0ab5d4ba316f
Alexandre Perez <alexandre.perez92@gmail.com>
2015-12-04 17:20:14 -0800

    Add custom keywords

--- lib/jazzy/jazzy_markdown.rb
+++ lib/jazzy/jazzy_markdown.rb
@@ -35,6 +35,7 @@ module Jazzy
                             Postcondition
                             Precondition
                             Project
+                            Protocol
                             Remark
                             Requires
                             Returns
